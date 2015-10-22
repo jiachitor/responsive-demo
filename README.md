cee70a1ef9c7088f0bee6e8843a7c85bc0b96c44
dolphin-news <jchliu@bainainfo.com>
2015-10-22 14:35:09 +0800

    first update

---
+++ README.md
@@ -0,0 +1,30 @@
+#iSlider-H5，Smooth slider for webapps
+iSlider-H5 from the [iSlider](https://github.com/be-fe/iSlider).
+
+##Get iSlider-H5
+- You could download the sourcecode form github here.
+- Using `npm install islider-h5`
+
+##iSlider-H5 Mobile Demo
+
+````
+gulp watch
+````
+注意：如果需要调试某种效果，可以在 gulpfile.js 文件中配置 demo_config 选项。
+
+##Getting Started
+````
+import iSlider from 'islider-h5'; 
+````
+or
+````
+var iSlider = require('islider-h5'); 
+````
+说明：因为此版本使用ES6 开发，所以目前只能在ES6环境中使用。如果需要以script 引入的方式使用，请使用 islider 官方原版。
+
+##翻页效果
+目前的翻页效果为"default","rotate","flip","depth","flow","card"，"fade","yrks","zxfd","jyxs","phyc","sxhd","kpfy"。如有需要，你可以自己填充动画库。
+
+
+##License (MIT)
+
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var fs = require("fs");
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var assign = require('lodash.assign');
+var notify = require('gulp-notify');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var less = require('gulp-less');
+
+var demo_config_list = ['demo'];
+var project = 'demo';
+
+function browserSyncTask(callback) {
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./src/" + project
+        }
+    });
+    callback();
+}
+
+function jsTask(callback) {
+    // Serve files from the root of this project
+    browserSync.stream();
+}
+
+function sassTask(callback) {
+    // Serve files from the root of this project
+    gulp.src(['src/' + project + '/sass/main.scss'])
+        .pipe(sass())
+        .on('error', gutil.log) 
+        .pipe(rename('app.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('src/' + project + '/css/'))
+        .pipe(browserSync.stream({stream: true}))
+        .pipe(notify('app.css to build complete'))  
+        .on('finish', callback);
+}
+
+function lessTask(callback) {
+    // Serve files from the root of this project
+    gulp.src(['src/' + project + '/less/main.less'])
+        .pipe(less())
+        .on('error', gutil.log) 
+        .pipe(rename('app.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('src/' + project + '/css/'))
+        .pipe(browserSync.stream({stream: true}))
+        .pipe(notify('app.css to build complete'))        
+        .on('finish', callback);
+}
+
+function watchTask() {
+    gulp.watch(['src' + project + '/js/*.js'], browserSync.reload);
+    gulp.watch(['src/' + project + '/less/*.less'], gulp.series(lessTask));
+}
+
+gulp.task('watch', gulp.series(
+    browserSyncTask,
+    gulp.parallel(lessTask,watchTask)
+));
+
+
